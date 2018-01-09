b7490796235a45decc3a43c583c0b0c10dfacbfb
Jered Sweeney <sweeneyjered@gmail.com>
2018-01-09 13:26:40 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -12,4 +12,3 @@ class Pirate
   end
 
 end
-
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,14 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@ships = []
+
+  def initialize(params)
+    @name, @type, @booty = params[:name], params[:type], params[:booty]
+    self.all << self
+  end
+
+  def self.all
+    @@ships
+  end
+
 end
