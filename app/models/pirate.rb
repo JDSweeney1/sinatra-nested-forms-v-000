aa4872b8bb58b320ca7753f0c06f670525a93515
Jered Sweeney <sweeneyjered@gmail.com>
2018-01-09 13:23:57 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@pirates = []
+
+  def initialize(params)
+    @name, @weight, @height = params[:name], params[:weight], params[:height]
+  end
+    
 end
